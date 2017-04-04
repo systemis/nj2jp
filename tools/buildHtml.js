b4982602d5ad4976430d23e29b6d8a622df978d2
TobiahRex <tobiah.rex@gmail.com>
2017-04-04 16:27:16 +0900

    added all 3 bundle files to buildHtml.js

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -2,17 +2,29 @@ import fs from 'fs';
 import cheerio from 'cheerio';
 import colors from 'colors';  // eslint-disable-line
 
-fs.readFile('src/index.html', (err, markup) => {
+fs.readFile('tools/index.template.html', (err, markup) => {
   process.stdout.write('\n');
   if (err) return process.stdout.write(`❌  fs.readFile ERROR: ${err}`);
 
   const $ = cheerio.load(markup);
-  $('#app').append('<script src="/commons.js" />');
 
-  fs.writeFile('dist/index.html', $.html(), 'utf8', (error) => {
-    process.stdout.write('\n');
-    if (error) return process.stdout.write(`❌  fs.readFile ERROR: ${error}`.red.bold);
-    return process.stdout.write('\n ✅  index.html written to "/dist"\n'.green.bold);
-  });
+  if (process.env.NODE_ENV === 'development') {
+    $('body')
+    .append('<script src="/bundle.js" />')
+    .append('<script src="/bundle.app.js" />')
+    .append('<script src="/bundle.vendor.js" />');
+    fs.writeFile('src/index.html', $.html(), 'utf8', (error) => {
+      process.stdout.write('\n');
+      if (error) return process.stdout.write(`❌  fs.readFile ERROR: ${error}`.red.bold);
+      return process.stdout.write('\n ✅  index.html written to "/src"\n'.green.bold);
+    });
+  } else {
+    $('#app').append('<script src="/commons.js" />');
+    fs.writeFile('dist/index.html', $.html(), 'utf8', (error) => {
+      process.stdout.write('\n');
+      if (error) return process.stdout.write(`❌  fs.readFile ERROR: ${error}`.red.bold);
+      return process.stdout.write('\n ✅  index.html written to "/dist"\n'.green.bold);
+    });
+  }
   return 1;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -113,7 +113,7 @@ const prodConfig = {
       },
       inject: true,
       title: 'Nic Juice 2 Japan',
-      filename: 'dist/index.html',
+      filename: './index.html',
     }),
     new CommonsChunkPlugin({
       name: 'commons',
